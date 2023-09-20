a172c037c6ffd8787ecc66c5f7f220eb86b7e0ea
tylerfortune1027 <144830588+tylerfortune1027@users.noreply.github.com>
2023-09-20 13:59:12 -0400

    Create MathEx.java

---
+++ MathEx.java
@@ -0,0 +1,12 @@
+public class MathEx {
+    public static void main (String [] args){
+        System.out.println(swap(123));
+    
+    }
+    public static int swap(int x){
+        int first = x%10*100;
+        int second = (x%100/10)*10;
+        
+    }
+    
+    }
