dde1aef561debda8feb0532448c174a9e7628e8e
tylerfortune1027 <tylerfortune@billericak12.com>
2023-10-04 13:51:12 -0400

    box

--- Box.java
+++ Box.java
@@ -22,6 +22,29 @@ public class Box {
         color = "purple";
         hasALid = true;
     }
+    public double getHeight(){
+        return height;
+    }
+    public void setHeight(double h){
+        if(h>0){
+            height=h;
+        }
+    }
+    public double volume(){
+        double v = length * width * height;
+        return v;
+    }
+    public String toString(){
+        String s = " a box with dimension " + length + "+ width + " + height;
+        s+= "\nColor is " + color + " has a lid? " + hasALid;
+        return s;
+    }
+    public boolean isTaller(Box other){
+        double h = other.getHeight();
+        if (height > h) return true;
+        return false;
+
+    }
     // default contructor
     public Box(){
         length = 1.0;
@@ -36,7 +59,19 @@ public class Box {
         Box b1 = new Box(6.5, 8.3, 9.4,"brown", false);
         Box b2 = new Box(3.6,2.8,7.4);
         Box b3 = new Box();
+        b3.setHeight(5.3);
+        System.out.println(b3.getHeight());
+        System.out.println(b1.getHeight());
+        double v1 = b1.volume();
+        System.out.println(" The volume of box 1 " +v1);
+        double v2 = b2.volume();
+        System.out.println(" The volume of box 2 " +v2);
 
-
+        //print the box
+        System.out.println(b1);
+        System.out.println(b2);
+        
+        boolean f = b1.isTaller(b3);
+        System.out.println("is b1 taller than b3 " + f);
     }
 }
