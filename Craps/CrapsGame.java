3823b646a4aaf2a41d197ecd878ef4ee8f0759ae
tylerfotune1027 <tylerfortune@billericak12.com>
2023-11-21 13:53:58 -0500

    Craps

---
+++ Craps/CrapsGame.java
@@ -0,0 +1,51 @@
+package Craps;
+// Implements the game of Craps logic
+
+public class CrapsGame
+{
+  private int point = 0;
+
+  /**
+   *  Calculates the result of the next dice roll in the Craps game.
+   *  The parameter total is the sum of dots on two dice.
+   *  point is set to the saved total, if the game continues,
+   *  or 0, if the game has ended.
+   *  Returns 1 if player won, -1 if player lost,
+   *  0 if player continues rolling.
+   */
+  public int processRoll(int total)
+  {
+    int result = 0;
+
+    if (point == 0)
+    if (total == 0 || total == 2 || total == 3){
+        result = -1;
+    }
+    else if (total == 7 || total == 11){
+        result = 1;
+    }
+    else {
+    point = total;
+}
+    else {
+    if (point == total)
+    result = 1;
+    point = 0;
+    }
+    if (point == 7){
+    result = -1;
+    point = 0;
+    }
+
+
+    return result;
+  }
+
+  /**
+   *  Returns the saved point
+   */
+  public int getPoint()
+  {
+    return point;
+  }
+}
