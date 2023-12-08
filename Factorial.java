25a5fe7d1b00917dab0baaa8878988f2b737987e
tylerfortune1027 <tylerfortune@billericak12.com>
2023-12-08 13:51:26 -0500

    avbc

---
+++ Factorial.java
@@ -0,0 +1,23 @@
+public class Factorial {
+    
+    public static long calcFactorial( int n){
+        long x = 1;
+        
+        for (int i = 1; i <= n ; i++ ){
+           x *= i;
+        }
+        return x;
+    }
+
+    public static 
+
+
+
+    public static void main(String[] args) {
+       Factorial ab = new Factorial();
+        for 
+        
+        System.out.println(calcFactorial());
+        
+}
+}
