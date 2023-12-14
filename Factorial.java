def5bd2cee8900cadc3832097aec84eced629b7a
tylerfortune1027 <tyerfortune@billericak12.com>
2023-12-14 13:53:31 -0500

    xyxl

--- Factorial.java
+++ Factorial.java
@@ -9,15 +9,47 @@ public class Factorial {
         return x;
     }
 
-    public static 
+    public static double calcE(){
+        double E = 1;
+        double oldE = 0;
+        int i = 1;
 
+        while(E - oldE > .001){
+            oldE = E;
+            E += (1.0 / calcFactorial(i));
+            i++;
+        }
+        return E;
+    }
 
+    public static double calcEX(int n){
+        double E = 1; 
+        double oldE = 0;
 
+        for(int i = 1; E - oldE > .001; i++){ 
+            oldE = E; 
+            E += (Math.pow(n, i) / calcFactorial(i)); 
+        }
+
+        return E;
+
+    }
     public static void main(String[] args) {
-       Factorial ab = new Factorial();
-        for 
-        
-        System.out.println(calcFactorial());
-        
+        System.out.println("Q1: \n");
+         for(int i = 1; i<= 20; i++)
+        System.out.println(calcFactorial(i));
+         System.out.println("\n");
+
+        System.out.println("Q2: \n");
+        System.out.println(calcE());
+        System.out.println("\n");
+
+
+        System.out.println("Question 3: \n");
+        for(int i = 1; i<= 5; i++)
+        System.out.printf("e is %2.3f \n", calcEX(i));
+        System.out.println("\n");
+    }
+
 }
-}
+  
