b960af5a4ffcc0b11f024fb2585d92894ff6300a
tylerfortune1027 <tyerfortune@billericak12.com>
2024-01-17 13:52:22 -0500

    cscscs

--- StringsExcersises.java
+++ StringsExcersises.java
@@ -11,27 +11,70 @@ public static String convertName(String s){
         String firstName = s.substring(comma + 1); 
         return firstName + " " + lastName; 
     }
-    public static String negative(String s){
-        return s.replace("0", "2").replace("1", "0").replace("2", "1");
+    // public static String negative(String s){
+
+    // }
+public static String dateString (String s){
+    int sl1 = s.indexOf("/"); 
+    int sl2 = s.indexOf("/", sl1 + 1); 
+    String mon = s.substring (0,sl1);
+    String day = s.substring (sl1 + 1, sl2);
+    String yr = s.substring (sl2 + 1 );
+ 
+    return day + "-" + mon + "-" + yr;
+}
+public static String dateString2 (String s){
+    int sl1 = s.indexOf("/"); 
+    int sl2 = s.indexOf("/", sl1 + 1); 
+    String mon = s.substring (0,sl1);
+    String day = s.substring (sl1 + 1, sl2);
+    String yr = s.substring (sl2 + 1 );
+
+    if (mon.length() == 1){
+        mon = "0" + mon;
+    }
+    if (day.length()== 1){
+        day = "0" + day;
+    }
+    return day + "-" + mon + "-" + yr;
+}
+
+// public static boolean startsWith (String s, String suffix){
+    
+// }
+// public static boolean endsWith (String s, String prefix){
+    
+// }
+
 public static void main(String[] args) {
 
+    System.out.println("Q1: ");
     System.out.println(scroll("Hello World"));
     System.out.println(scroll("happy"));
     System.out.println(scroll("h"));
+    System.out.println();
 
+    System.out.println("Q2: ");
     System.out.println(convertName(" Reubenstein, Lori Renee  	"));
     System.out.println(convertName("Biden,Joe"));
     System.out.println(convertName("the Clown, Bozo"));
+    System.out.println();
 
-    System.out.println(negative("0010111001"));
-    System.out.println(negative("11111111"));
+    // System.out.println("Q3: ");
+    // // System.out.println(negative("0010111001"));
+    // // System.out.println(negative("11111111"));
+    // System.out.println();
 
-    // System.out.println("04/20/2014 becomes " + dateString("04/20/2014"));
+    System.out.println( "Q4: ");
+    System.out.println("04/20/2014 becomes " + dateString("04/20/2014"));
+    System.out.println();
 
-    // System.out.println("04/20/2014 becomes " + dateString2("04/20/2014"));
-    // System.out.println("4/20/2014 becomes " + dateString2("4/20/2014"));
-    // System.out.println("04/2/2014 becomes " + dateString2("04/2/2014"));
-    // System.out.println("4/2/2024 becomes " + dateString2("4/2/2024"));
+    System.out.println( "Q5: ");
+    System.out.println("04/20/2014 becomes " + dateString2("04/20/2014"));
+    System.out.println("4/20/2014 becomes " + dateString2("4/20/2014"));
+    System.out.println("04/2/2014 becomes " + dateString2("04/2/2014"));
+    System.out.println("4/2/2024 becomes " + dateString2("4/2/2024"));
+    System.out.println();
 
     // System.out.println("\nstartsWith");
     // System.out.println(startsWith("architecture", "arch"));
