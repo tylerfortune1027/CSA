b2a4f5400a65fd8c4ec0b46a2c325d35873bdba8
tylerfortune1027 <tyerfortune@billericak12.com>
2024-02-15 13:34:27 -0500

    ssss

---
+++ Complex.java
@@ -0,0 +1,62 @@
+public class Complex {
+    private double a;
+    private double b;
+
+
+    public Complex(double a, double b) { 
+        this.a = a;
+        this.b = b;
+    }
+
+
+    public Complex(double a) { 
+        this(a ,0.0);
+    }
+
+
+    public double abs() {
+        if (b == 0.0) return (Math.sqrt(a*a + b*b));
+        return b;
+    }
+
+
+    public Complex add(Complex other) {
+        return new Complex((a + other.a),(b + other.b));
+    }
+
+
+    public String toString() {
+        return a + "+" + b + "i";
+    }
+
+
+    public Complex multiply(Complex other) {
+        return new Complex((a * other.a) - (a * other.b), (b * other.a) - (b * other.b));
+    }
+
+
+    public Complex add1(double x) { 
+        return new Complex((a + x),(b + x));
+    }
+
+
+    public Complex multiply1(double x) { 
+        return new Complex((a * x), (b * x));
+    }
+    public static void main(String[] args) {
+        Complex c = new Complex(3,5);
+        Complex c2 = new Complex(4,1);
+        System.out.println("\nabs");
+        System.out.println(c.abs());
+        System.out.println("\nadd");
+        System.out.println(c.add(c2));
+        System.out.println("\ntoString");
+        System.out.println(c.toString());
+        System.out.println("\nmultiply");
+        System.out.println(c.multiply(c2));
+        System.out.println("\nadd1");
+        System.out.println(c.add1(4.0));
+        System.out.println("\nmultiply1");
+        System.out.println(c.multiply1(4.0));
+}
+}
--- SoccerTeam.java
+++ SoccerTeam.java
@@ -6,7 +6,7 @@ public class SoccerTeam {
     private static int gamesPlayed = 0;
     private static int goalsScored= 0;
 
-    public SoccerTeam (int score){
+    public SoccerTeam (){
         this.numWins = 0;
         this.numLosses = 0;
         this.ties = 0;
@@ -29,6 +29,10 @@ public class SoccerTeam {
     public int getPoints(){
         return 3 * this.numWins + this.ties;
     }
+    public int points(){
+        return (numWins * 3) + (ties * 1);
+    }
+
     
     public void reset(){
         this.numWins= 0;
@@ -43,12 +47,56 @@ public int getGoalsScored(){
 
 }
 public void startTournament(){
-    
+    SoccerTeam.gamesPlayed = 0;
+    SoccerTeam.goalsScored = 0;
+
 }
 
       
     public static void main(String[] args) {
             
+            System.out.println("Tournament 1: \n");
+            SoccerTeam team1 = new SoccerTeam();
+            SoccerTeam team2 = new SoccerTeam();
+            team1.played(team2, 3, 2);
+            System.out.println("Team 1 points: " + team1.points());
+            System.out.println("Team 2 points: " + team2.points());
+            System.out.println("Games played: " + SoccerTeam.getGamesPlayed());
+            System.out.println("Goals scored: " + SoccerTeam.getGoalsScored());
+            System.out.println("\n");
+           
+            System.out.println("Tournament 2: \n");
+            team1.reset();
+            team2.reset();
+            team1.played(team2, 1, 1);
+            System.out.println("Team 1 points: " + team1.points());
+            System.out.println("Team 2 points: " + team2.points());
+            System.out.println("Games played: " + SoccerTeam.getGamesPlayed());
+            System.out.println("Goals scored: " + SoccerTeam.getGoalsScored());
+            System.out.println("\n");
+            
+            System.out.println("Tournament 3: \n");
+            team1.reset();
+            team2.reset();
+            team1.played(team2, 0, 2);
+            System.out.println("Team 1 points: " + team1.points());
+            System.out.println("Team 2 points: " + team2.points());
+            System.out.println("Games played: " + SoccerTeam.getGamesPlayed());
+            System.out.println("Goals scored: " + SoccerTeam.getGoalsScored());
+            System.out.println("\n");
+
+            System.out.println("Tournament 4: \n");
+            team1.reset();
+            team2.reset();
+            team1.played(team2, 2, 2);
+            System.out.println("Team 1 points: " + team1.points());
+            System.out.println("Team 2 points: " + team2.points());
+            System.out.println("Games played: " + SoccerTeam.getGamesPlayed());
+            System.out.println("Goals scored: " + SoccerTeam.getGoalsScored());
+            System.out.println("\n");
+        }    
+    }
+    
 
         }
 }
