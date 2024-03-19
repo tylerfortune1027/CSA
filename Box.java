83248a3ba470ef0254f02cd87e52f1662276a09c
tylerfortune1027 <tyerfortune@billericak12.com>
2024-03-19 12:58:32 -0400

    sss

--- Box.java
+++ Box.java
@@ -1,77 +1,17 @@
-public class Box {
-    private double length;
-    private double width;
-    private double height;
-    private String color;
-    private boolean hasALid;
 
-    // constructor - a method that creates/ initializes an instance//
-    // of a class
-    public Box(double l, double w, double h, String c, boolean lid){
-        length = l;
-        width = w;
-        height = h;
-        color = c;
-        hasALid = lid;
-    }
-    //overload the constructor
-    public Box(double l, double w, double h){
-        length = l;
-        width = w;
-        height = h;
-        color = "purple";
-        hasALid = true;
-    }
-    public double getHeight(){
-        return height;
-    }
-    public void setHeight(double h){
-        if(h>0){
-            height=h;
-        }
-    }
-    public double volume(){
-        double v = length * width * height;
-        return v;
+public class Box {
+    private boolean isBlack;
+    private int num;
+
+    public Box(boolean isb, int n){
+        isBlack = isb;
+        num = n;
     }
     public String toString(){
-        String s = " a box with dimension " + length + " "+ width + " " + height;
-        s+= "\nColor is " + color + " has a lid? " + hasALid;
+        String s;
+        if(isBlack)s=" black";
+        else if(num>0)s="  "+num+"wh";
+        else s=" white";
         return s;
-    }
-    public boolean isTaller(Box other){
-        double h = other.getHeight();
-        if (height > h) return true;
-        return false;
-
-    }
-    // default contructor
-    public Box(){
-        length = 1.0;
-        width = 1.0;
-        height = 1.0;
-        color = "white";
-        hasALid = false;
-
-    }
-    public static void main (String[] args){
-        // create and instance
-        Box b1 = new Box(6.5, 8.3, 9.4,"brown", false);
-        Box b2 = new Box(3.6,2.8,7.4);
-        Box b3 = new Box();
-        b3.setHeight(5.3);
-        System.out.println(b3.getHeight());
-        System.out.println(b1.getHeight());
-        double v1 = b1.volume();
-        System.out.println(" The volume of box 1 " +v1);
-        double v2 = b2.volume();
-        System.out.println(" The volume of box 2 " +v2);
-
-        //print the box
-        System.out.println(b1);
-        System.out.println(b2);
-        
-        boolean f = b1.isTaller(b3);
-        System.out.println("is b1 taller than b3? " + f);
     }
 }
